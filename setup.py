bb3d4651b21279570bfdf1a20f5a47b7295a48a4
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-07-21 21:46:19 -0700

    Add initial framework setup from accelerate

--- setup.py
+++ setup.py
@@ -14,10 +14,8 @@
 
 from setuptools import find_packages, setup
 
-
 extras = {}
 extras["quality"] = ["ruff == 0.13.1"]
-
 extras["docs"] = []
 extras["test_prod"] = ["pytest>=7.2.0", "pytest-xdist", "pytest-subtests", "parameterized", "pytest-order"]
 extras["test_dev"] = [
@@ -36,21 +34,18 @@ extras["test_dev"] = [
 extras["testing"] = extras["test_prod"] + extras["test_dev"]
 extras["deepspeed"] = ["deepspeed"]
 extras["rich"] = ["rich"]
-
-extras["test_fp8"] = ["torchao"]  # note: TE for now needs to be done via pulling down the docker image directly
+extras["test_fp8"] = ["torchao"]
 extras["test_trackers"] = [
     "wandb",
     "tensorboard",
     "dvclive",
-    # "mlflow", too many deps that lead to download a very old version of the lib
     "matplotlib",
-    "swanlab[dashboard]",  # dashboard required for local use
+    "swanlab[dashboard]",
     "trackio",
 ]
 extras["dev"] = extras["quality"] + extras["testing"] + extras["rich"]
-
 extras["sagemaker"] = [
-    "sagemaker",  # boto3 is a required package in sagemaker
+    "sagemaker",
 ]
 
 setup(

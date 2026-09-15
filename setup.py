19e5f950938dd839e85a511580b1dbfe52b596ec
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-09-15 16:54:39 -0700

    Copy setup.py from meta-llama/llama for Jane

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
 from setuptools import find_packages, setup
 
+
+def get_requirements(path: str):
+    return [l.strip() for l in open(path)]
+
+
 setup(
-    name="modelhub",
+    name="llama",
     version="0.0.1",
-    description="ModelHub - framework for LLM research",
     packages=find_packages(),
-    python_requires=">=3.10",
-    install_requires=[],
+    install_requires=get_requirements("requirements.txt"),
 )
